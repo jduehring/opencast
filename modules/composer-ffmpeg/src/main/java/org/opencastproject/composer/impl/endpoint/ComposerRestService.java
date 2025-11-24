d9413ef70fd1b593c1f6e9fb59eaa82b47bb944a
Martin Wygas <wygas@elan-ev.de>
2025-11-24 10:12:33 +0100

    Remove unused imports

--- modules/composer-ffmpeg/src/main/java/org/opencastproject/composer/impl/endpoint/ComposerRestService.java
+++ modules/composer-ffmpeg/src/main/java/org/opencastproject/composer/impl/endpoint/ComposerRestService.java
@@ -62,7 +62,6 @@ import org.osgi.service.jaxrs.whiteboard.propertytypes.JaxrsResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -83,7 +82,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import jdk.javadoc.doclet.Reporter;
 
 /**
  * A REST endpoint delegating functionality to the {@link ComposerService}
